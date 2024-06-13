300fa92b848b31188c2c34d6066b54ffc9ac76d9
Rolando Guzman <rolando.guzman@agrisem.com.mx>
2024-06-12 17:38:43 -0700

    antes de probar el metodo de array

--- html/Contenido/utilerias.js
+++ html/Contenido/utilerias.js
@@ -40,6 +40,24 @@ const IS_MOBILE = () => {
     );
 }
 /**
+ * @param {string} numero 
+ * @returns true si el parametro es un numero y es mayor que cero, de lo contrario retorna false.
+ */
+function esNumeroMayorQueCero(numero) {
+    let Okay = true;
+    try {
+        if(isNaN(numero)){
+            Okay = false;
+        } else if(Number(numero) <= 0){
+            Okay = false;
+        }
+    } catch (error) {
+        console.error("esNumeroMayorQueCero => ", error);
+        Okay = false;
+    }
+    return Okay;
+}
+/**
  * Convierte una cadena a un número si es mayor que cero, de lo contrario retorna 0.
  * @param {string} strValor - La cadena a convertir.
  * @returns {number} El número convertido o 0 si no es mayor que cero.
@@ -53,12 +71,34 @@ const convertStringToNumber = (strValor) => {
     }
 }
 /**
+ * @param {string} StringVal Cadena 
+ * @returns True si cumple con las condiones de estar vacio, nulo, undefined
+ */
+function IsNullOrEmpty(StringVal){
+    let tipo = typeof(StringVal),
+        retorno = false;
+
+    if(StringVal === null || StringVal === undefined){
+        return true;
+    }
+
+    if(tipo == "undefined" || tipo == "null"){
+        retorno = true;
+    } else if(tipo == "string" || tipo == "number") {
+        if(StringVal.toString().trim() == ""){
+            retorno = true;
+        }
+    }
+
+    return retorno;
+}
+/**
  * Valida que una cadena sea un numero y que tenga la cantidad maxima de decimales permitidos.
  * @param {string} valor valor que se va a validar.
  * @param {number} maxDecimales Es la cantidad maxima de decimales permitidos, por default son 3.
  * @returns True si la validacion es correcta y False cuando no se cumple con la condición.
  */
-const ValidarDecimales = (valor, maxDecimales = 3) => {
+const ValidarDecimales = (valor, maxDecimales = 2) => {
     try {
         // Expresión regular para validar un número con hasta 3 decimales
         const regex = new RegExp(`^\\d+(\\.\\d{1,${maxDecimales}})?$`);
@@ -80,3 +120,226 @@ const ValidarDecimales = (valor, maxDecimales = 3) => {
  * @returns True o False
  */
 const isArrayWithData = _array => _array && Array.isArray(_array) && _array.length > 0;
+
+/**
+ * Muestra un mensaje emergente y espera a que el usuario precione aceptar para continuar con la ejecucion del codigo.
+ * @param {object} oMensaje {Titulo: '', Mensaje: '', Tipo: ''}
+ */
+const mostrarAlertaAsync = async (oMensaje) => {
+    const TITULO = !oMensaje.Titulo ? '¡AVISO!' : oMensaje.Titulo.trim();
+    const MENSAJE = !oMensaje.Mensaje ? 'Revize los detalles de la operaci&oacute;n' : oMensaje.Mensaje.trim();
+    const TIPO_ALERTA = !oMensaje.Tipo ? 'warning' : oMensaje.Tipo.trim();
+
+    let ColoButton = "#F9A826";
+    switch (TIPO_ALERTA) {
+        case "success":
+            ColoButton = "#4b8629"
+            break;
+        case "error":
+            ColoButton = "#E64F4F"
+            break;
+        case "info":
+            ColoButton = "#2D92A2"
+            break;
+    }
+
+    await Swal.fire({
+        title: TITULO,
+        html: MENSAJE,
+        icon: TIPO_ALERTA,
+        focusConfirm: false,
+        allowEnterKey: false,
+        confirmButtonText: 'Aceptar',
+        confirmButtonColor: ColoButton,
+        allowOutsideClick: false,
+        didOpen: () => {
+            const isLoading = Swal.isLoading();
+            if(isLoading){
+                Swal.hideLoading();
+            }
+        }
+    });
+}
+
+const mostrarSuccess = (titulo, mensaje = '') => {
+    Swal.fire({
+        title: titulo,
+        html: mensaje.trim(),
+        icon: 'success',
+        focusConfirm: false,
+        allowEnterKey: false,
+        confirmButtonText: 'Aceptar',
+        confirmButtonColor: '#4b8629',
+        allowOutsideClick: false,
+        didOpen: () => {
+            const isLoading = Swal.isLoading();
+            if(isLoading){
+                Swal.hideLoading();
+            }
+        }
+    });
+}
+
+const mostrarWarning = (titulo, mensaje = '') => {
+    Swal.fire({
+        title: titulo,
+        html: mensaje.trim(),
+        icon: 'warning',
+        focusConfirm: false,
+        allowEnterKey: false,
+        confirmButtonText: 'Aceptar',
+        confirmButtonColor: '#F9A826',
+        allowOutsideClick: false,
+        didOpen: () => {
+            const isLoading = Swal.isLoading();
+            if(isLoading){
+                Swal.hideLoading();
+            }
+        }
+    });
+}
+
+const mostrarError = (titulo, mensaje = '') => {
+    Swal.fire({
+        title: titulo,
+        html: mensaje.trim(),
+        icon: 'error',
+        focusConfirm: false,
+        allowEnterKey: false,
+        confirmButtonText: 'Aceptar',
+        //imageHeight: 150,
+        confirmButtonColor: '#E64F4F',
+        //imageUrl: ImgBase64_Error(),
+        allowOutsideClick: false,
+        didOpen: () => {
+            const isLoading = Swal.isLoading();
+            if(isLoading){
+                Swal.hideLoading();
+            }
+        }
+    });
+}
+
+const mostrarInfo = (titulo, mensaje = '', TextoDelBoton = 'Aceptar') => {
+    Swal.fire({
+        title: titulo,
+        html: mensaje.trim(),
+        icon: 'info',
+        focusConfirm: false,
+        allowEnterKey: false,
+        //confirmButtonText: 'Aceptar',
+        confirmButtonColor: '#2D92A2',
+        confirmButtonText: TextoDelBoton,
+        allowOutsideClick: false,
+        didOpen: () => {
+            const isLoading = Swal.isLoading();
+            if(isLoading){
+                Swal.hideLoading();
+            }
+        }
+    });
+}
+/**
+ * @param {string} mensaje Texto a mostrar
+ * @param {string} PosicionPantalla 'top', 'top-start', 'top-end', 'center', 'center-start', 'center-end', 'bottom', 'bottom-start', or 'bottom-end'
+ */
+const mostrarMensajeNull = (mensaje = '', PosicionPantalla = 'center') => {
+    mensaje = `<span style="font-size: 20px;">${$.trim(mensaje)}</span>`;
+
+    Swal.fire({
+        title: mensaje,
+        confirmButtonText: 'Aceptar',
+        confirmButtonColor: "#44a277",
+        position: PosicionPantalla,
+        allowOutsideClick: false,
+        allowEscapeKey   : false,
+        didOpen: () => {
+            const isLoading = Swal.isLoading();
+            if(isLoading){
+                Swal.hideLoading();
+            }
+        }
+    })
+}
+
+const mostrarMensajeNullConFuncion = (fnAceptar, fnCancelar, mensaje = '', textButtonConfirm = 'Aceptar', textDebyButton = 'Cancelar') => {
+    mensaje = `<span style="font-size: 20px;">${$.trim(mensaje)}</span>`;
+    Swal.fire({
+        showCancelButton  : false,
+        showDenyButton    : true,
+        title             : mensaje,
+        confirmButtonColor: '#4088a6',
+        denyButtonColor   : '#b0b6bb',
+        confirmButtonText : textButtonConfirm,
+        denyButtonText    : textDebyButton,
+        allowOutsideClick : false,
+        allowEscapeKey    : false,
+        reverseButtons    : true,
+        allowEnterKey     : false,
+        didOpen: () => {
+            const isLoading = Swal.isLoading();
+            if(isLoading){
+                Swal.hideLoading();
+            }
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fnAceptar
+        } else if (result.isDenied) {
+            fnCancelar
+        }
+    });
+}
+
+/**
+ * @description Muestra un modal con mensaje personalizado que le indica al usuario un momento de espera.
+ * @param {string} texto Es el texto del titulo del mensaje
+ * @param {string} Mensaje Sub mensaje a mostrar
+ * @param {boolean} BloqueoGlobal Indica si el bloqueo sera total en la pantalla o solo en Frame donde se llama el bloqueo.
+ */
+const MostrarBLoqueo = (texto = 'Un momento...', Mensaje = '', BloqueoGlobal = true) => {
+    BloqueoSwal(texto, Mensaje);
+}
+
+const BloqueoSwal = (texto = 'Un momento...', Mensaje = '') => {
+    Swal.fire({
+        title            : texto,
+        html             : `<span style = "color: #8d8f91;">${Mensaje}</span>`,
+        allowOutsideClick: false,
+        allowEscapeKey   : false,
+        showConfirmButton: false,
+        didOpen: () => {
+            Swal.showLoading()
+        }
+    })
+}
+
+const MostrarMensajeConBloqueo = (TipoMensaje, titulo, Mensaje) => {
+    Swal.fire({
+        icon: TipoMensaje,
+        title: titulo,
+        html: Mensaje,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        showConfirmButton: false,
+    })
+}
+
+const CerrarBloqueo = (BloqueoGlobal = true) => {
+    CerrarBloqueoSwal();
+}
+
+const CerrarBloqueoSwal = () => {
+    if(Swal.isVisible()){
+        Swal.close()
+    }
+}
+function generaIdUnico() {
+    let d = new Date().getTime(),
+        uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            let r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    return uuid;
+}
--- html/index.js
+++ html/index.js
@@ -1,15 +1,41 @@
 const app = new Vue({
     el: '#app',
     data: {
+        decimales: 2,
+        tabSelected: 'Maíz Blanco',
         isMobil: IS_MOBILE(),
         oValor: null,
-        oDataMaiz: {
+        Maiz: {
             Semilla:{
-                Unidad: ''
-            }
+                Unidad: {
+                    Valor: '',
+                    Error: ''
+                }
+            },
+            Fertilizante: {
+                Costo:{
+                    Valor: "",
+                    Error: ""
+                }
+            },
+            SeguroAgricola: {
+                Costo:{
+                    Valor: "",
+                    Error: ""
+                }
+            },
+            PagoAgua: {
+                Costo:{
+                    Valor: "",
+                    Error: ""
+                }
+            },
         }
     },
     methods: {
+        focusElement: function(element){
+            $(element).focus();
+        },
         FormatoNumero: function(NumberString = ''){
             try {
                 return formatoNumeroMiles(NumberString);                
@@ -18,24 +44,168 @@ const app = new Vue({
                 return "0.00";
             }
         },
+        /**
+         * Valida que se ingresen solo valores decimales en el campo de texto.
+         * @param {*} $event 
+         * @param {number} DecimalesAdmitidos 
+         */
+        SoloDecimales: function($event, DecimalesAdmitidos = 2) {
+            try {
+                let keyCode = ($event.keyCode ? $event.keyCode : $event.which),
+                    el = $($event.target),
+                    valor = el.val(),
+                    FindPunto = valor.indexOf('.');
+
+                if ((keyCode < 48 || keyCode > 57) && (keyCode !== 46 || FindPunto != -1)) {
+                    $event.preventDefault();
+                }
+
+                if(isNaN(DecimalesAdmitidos)){
+                    DecimalesAdmitidos = 2;
+                }
+
+                if(Number(DecimalesAdmitidos) <= 0){
+                    DecimalesAdmitidos = 2;
+                }
+
+                const TotalDecimales = !IsNullOrEmpty(valor) && valor.toString().includes(".") ? valor.split('.')[1].length : 0;
+                if(valor != null && FindPunto > -1 && (TotalDecimales === DecimalesAdmitidos)){
+                    $event.preventDefault();
+                }   
+            } catch (error) {
+                console.error(`SoloDecimales => ${error}`);
+            }
+        },
         ObtenerCantidadesFile: async function(){
             try {
-                const rutaValores = "./Contenido/valores.json";
-                let errorAxios = null;
-                const res = await axios.get(rutaValores).catch(error => {errorAxios = error; });                
-                if (errorAxios) { throw errorAxios } 
-                const oValoresFromFileJson = res.data;
-                if(!oValoresFromFileJson || typeof(oValoresFromFileJson) !== 'object'){throw 'Los valores son incorrectos.'}
-                this.oValor = $.extend(true, {}, oValoresFromFileJson);
-                console.log(this.oValor)
+                MostrarBLoqueo();
+                const pathFileJson = "./Contenido/valores.json";
+                fetch(pathFileJson)
+                .then(response => response.json())
+                .then(data => {
+                    const oValoresFromFileJson = data;
+                    if(!oValoresFromFileJson || typeof(oValoresFromFileJson) !== 'object'){throw 'Los valores son incorrectos.'}
+                    this.oValor = $.extend(true, {}, oValoresFromFileJson);
+                    this.decimales = esNumeroMayorQueCero(oValoresFromFileJson?.DecimalesAdmitidos) 
+                                    ? convertStringToNumber(oValoresFromFileJson.DecimalesAdmitidos)
+                                    : 2;
+                    console.log(this.oValor);
+                    setTimeout(() => {
+                        CerrarBloqueo();
+                    }, 300);
+                })
+                .catch(error => {
+                    mostrarError(
+                        "ALGO SALIO MAL", 
+                        "Error al leer los valores de los cultivos. <p class='mb-0'>" + error + "</p>"
+                    );
+                });
             } catch (error) {
+                debugger;
                 console.error(`ObtenerCantidadesFile => ${error}`);
                 mostrarError("ALGO SALIO MAL", "No es posible obtener los valores iniciales.");
             }
-        }
+        },
+        ValidarUnidadSemillaMaiz: function(){
+            const x = this;
+            try {
+                x.Maiz.Semilla.Unidad.Error = '';
+                let value = x.Maiz.Semilla.Unidad.Valor;
+                if(value === ""){
+                    x.Maiz.Semilla.Unidad.Valor = 0;
+                } else if(isNaN(value)){
+                    x.Maiz.Semilla.Unidad.Error = 'Solo números.'
+                } else {
+                    if(!ValidarDecimales(value, x.decimales)){
+                        //Si hay mas decimales
+                        value = Number(value);
+                        x.Maiz.Semilla.Unidad.Valor = value.toFixed(x.decimales);
+                    }
+                }
+            } catch (error) {
+                console.error(`ValidarUnidadSemillaMaiz => ${error}`);
+                x.Maiz.Semilla.Unidad.Error = 'Error al validar.'
+            }
+        },
+        ValidarCostoFertilizanteMaiz: function(){
+            const x = this;
+            try {
+                x.Maiz.Fertilizante.Costo.Error = '';
+                let value = x.Maiz.Fertilizante.Costo.Valor;
+                if(value === ""){
+                    x.Maiz.Fertilizante.Costo.Valor = 0;
+                } else if(isNaN(value)){
+                    x.Maiz.Fertilizante.Costo.Error = 'Solo números.'
+                } else {
+                    if(!ValidarDecimales(value, x.decimales)){
+                        //Si hay mas decimales
+                        value = Number(value);
+                        x.Maiz.Fertilizante.Costo.Valor = value.toFixed(x.decimales);
+                    }
+                }
+            } catch (error) {
+                console.error(`ValidarCostoFertilizante => ${error}`);
+                x.Maiz.Fertilizante.Costo.Error = 'Error al validar.'
+            }
+        },
+        ValidarCostoSeguroAgricolaMaiz: function(){
+            const x = this;
+            try {
+                x.Maiz.SeguroAgricola.Costo.Error = '';
+                let value = x.Maiz.SeguroAgricola.Costo.Valor;
+                if(value === ""){
+                    x.Maiz.SeguroAgricola.Costo.Valor = 0;
+                } else if(isNaN(value)){
+                    x.Maiz.SeguroAgricola.Costo.Error = 'Solo números.'
+                } else {
+                    if(!ValidarDecimales(value, x.decimales)){
+                        //Si hay mas decimales
+                        value = Number(value);
+                        x.Maiz.SeguroAgricola.Costo.Valor = value.toFixed(x.decimales);
+                    }
+                }
+            } catch (error) {
+                console.error(`ValidarCostoFertilizante => ${error}`);
+                x.Maiz.SeguroAgricola.Costo.Error = 'Error al validar.'
+            }
+        },
+        ValidarCostoPagoAguaMaiz: function(){
+            const x = this;
+            try {
+                x.Maiz.PagoAgua.Costo.Error = '';
+                let value = x.Maiz.PagoAgua.Costo.Valor;
+                if(value === ""){
+                    x.Maiz.PagoAgua.Costo.Valor = 0;
+                } else if(isNaN(value)){
+                    x.Maiz.PagoAgua.Costo.Error = 'Solo números.'
+                } else {
+                    if(!ValidarDecimales(value, x.decimales)){
+                        //Si hay mas decimales
+                        value = Number(value);
+                        x.Maiz.PagoAgua.Costo.Valor = value.toFixed(x.decimales);
+                    }
+                }
+            } catch (error) {
+                console.error(`ValidarCostoPagoAguaMaiz => ${error}`);
+                x.Maiz.PagoAgua.Costo.Error = 'Error al validar.'
+            }
+        },
     },
     computed: {
-        
+        MaizSemillaCosto: function(){
+            try {
+                const unidad = convertStringToNumber(this.Maiz.Semilla.Unidad.Valor);
+                const PrecioSemilla = convertStringToNumber(this.oValor?.MaizBlanco?.PrecioSacoSemilla);
+                if(unidad > 0){
+                    return unidad * PrecioSemilla
+                } else {
+                    return 0
+                }
+            } catch (error) {
+                console.log(`MaizSemillaCosto => ${error}`);
+                return 0
+            }
+        }
     },
     mounted: async function (){
         try{
